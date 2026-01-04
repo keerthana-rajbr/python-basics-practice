e373b2e9d0dbcb5fbdcb750c0e5ac814f1472be5
Keerthana Raj B R <keerthanarajbr98@gmail.com>
2026-01-04 22:49:09 +0530

    Completed numeric programs 001 to 004

---
+++ python_100_numeric_programs/004_divide_two_numbers.py
@@ -0,0 +1,24 @@
+"""
+004 - Write a program to divide two numbers
+
+Testcases:
+Input: 4, 2
+Output: 2.0
+
+Input: 23, 0
+Output: Division by zero not allowed
+"""
+
+def divide_numbers(a,b):
+    if b == 0:
+        return None
+    return a / b
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+quotient = divide_numbers(num1,num2)
+
+if quotient is None:
+    print('Division by zero not allowed')
+else:
+    print(f'The Quotient is {quotient}')
