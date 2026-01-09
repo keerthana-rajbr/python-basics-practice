6515817483fff3986b59d00bff2128929e467aa7
Keerthana Raj B R <keerthanarajbr98@gmail.com>
2026-01-09 22:42:17 +0530

    completed numeric programs 021 to 028

--- python_100_numeric_programs/028_remove_first_digit_of_number.py
+++ python_100_numeric_programs/028_remove_first_digit_of_number.py
@@ -1,6 +1,20 @@
+"""
+028 - Write a program to remove the first digit from a number
 
+Testcases:
+Input: 435
+Output: 35
 
+Input: 100
+Output: 0
 
+Input: 0
+Output:0
+
+Input: -324
+Output: 24
+
+"""
 
 def digit_count(number):
     if number == 0:
@@ -21,4 +35,10 @@ def remove_first_digit(number):
 
 num = abs(int(input('Enter a number: ')))
 result = remove_first_digit(num)
-print(result)
+print(f'After removing the first digit, the number is {result}')
+
+
+
+
+
+
